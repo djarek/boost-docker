72b512ca100bd3f4a9ed93b9b604eb41ee6f4bdf
Damian Jarek <damian.jarek93@gmail.com>
2018-04-16 23:33:49 +0200

    Move building boost into get_boost.sh script
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- get_boost.sh
+++ get_boost.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 BOOST_URL=https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
 
-curl -L $BOOST_URL --output boost.tar.gz \
+(curl -L $BOOST_URL --output boost.tar.gz \
 && tar -xf boost.tar.gz \
-&& rm boost.tar.gz
+&& rm boost.tar.gz \
+&& cd boost_* \
+&& ./bootstrap.sh --prefix=$BOOST_PREFIX --with-toolset=$BOOST_TOOLSET \
+&& ./b2 --without-python install)
