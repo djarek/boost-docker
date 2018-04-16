d4ad9b43c061639ca441645405d3a5cbbc61f185
Damian Jarek <damian.jarek93@gmail.com>
2018-04-16 21:15:23 +0200

    Use variable in get_boost.sh
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- get_boost.sh
+++ get_boost.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 BOOST_URL=https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
 
-curl -L https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz --output boost.tar.gz \
+curl -L $BOOST_URL --output boost.tar.gz \
 && tar -xf boost.tar.gz \
 && rm boost.tar.gz
