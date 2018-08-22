ab4a808c66e98e5a41052cc42e0836893091f176
Damian Jarek <damian.jarek93@gmail.com>
2018-08-22 15:40:33 +0200

    Boost 1.68
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- gcc/7/get_boost.sh
+++ gcc/7/get_boost.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-BOOST_URL=https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+BOOST_URL=https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz
 
 (curl -L $BOOST_URL --output boost.tar.gz \
 && tar -xf boost.tar.gz \
